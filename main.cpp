c2bfad9e9a8166eceab6a27657ef3dac660e1fe7
shubh <shadowed.sp@gmail.com>
2025-05-01 21:31:41 +0530

    audio processing

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include "include/raylib.h"
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
 #include <iostream>
-int main() {
 
+uint32_t glo_frames[1024];
+size_t glo_frames_c = 0;
+
+void callback(void *bufferData, unsigned int frames) {
+  memcpy(glo_frames, bufferData, frames * sizeof(uint32_t) * frames * 2);
+  glo_frames_c = frames;
+}
+
+int main() {
   InitWindow(800, 600, "music");
   SetTargetFPS(60);
 
@@ -9,20 +20,25 @@ int main() {
   Music music = LoadMusicStream("song.mp3");
   PlayMusicStream(music);
   SetMusicVolume(music, 0.5f);
+  AttachAudioStreamProcessor(music.stream, callback);
 
   while (!WindowShouldClose()) {
     UpdateMusicStream(music);
 
     if (IsKeyPressed(KEY_SPACE)) {
       if (IsMusicStreamPlaying(music)) {
-        std::cout << "music stopped";
         PauseMusicStream(music);
       }
     } else {
       ResumeMusicStream(music);
     }
     BeginDrawing();
-    ClearBackground(RED);
+    ClearBackground(BLACK);
+    // float cell_w = (float)GetRenderWidth() / glo_frames_c;
+    for (size_t i = 0; i < glo_frames_c; ++i) {
+      int16_t sample = *(int16_t *)&glo_frames[i];
+      printf("%d\n", sample);
+    }
     EndDrawing();
   }
   return 0;
