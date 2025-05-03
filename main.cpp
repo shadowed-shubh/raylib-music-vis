43e69e683cdf3c4db3f7b464c9a8e6fa1d1f7421
shubh <shadowed.sp@gmail.com>
2025-05-03 21:19:14 +0530

    sample printing and audio analysis

--- main.cpp
+++ main.cpp
@@ -1,44 +1,44 @@
 #include "include/raylib.h"
+#include <cassert>
 #include <cstddef>
 #include <cstdint>
+#include <cstdio>
 #include <cstring>
-#include <iostream>
+
+#define ARR_len(xs) sizeof(xs) / sizeof(xs[0])
 
 uint32_t glo_frames[1024];
 size_t glo_frames_c = 0;
 
-void callback(void *bufferData, unsigned int frames) {
-  memcpy(glo_frames, bufferData, frames * sizeof(uint32_t) * frames * 2);
+void callback(void *bufferdata, unsigned int frames) {
+  if (frames > ARR_len(glo_frames)) {
+    frames = ARR_len(glo_frames);
+  }
+  memcpy(glo_frames, bufferdata, frames * sizeof(float));
   glo_frames_c = frames;
 }
-
 int main() {
   InitWindow(800, 600, "music");
   SetTargetFPS(60);
 
   InitAudioDevice();
   Music music = LoadMusicStream("song.mp3");
-  PlayMusicStream(music);
-  SetMusicVolume(music, 0.5f);
   AttachAudioStreamProcessor(music.stream, callback);
+
+  PlayMusicStream(music);
 
   while (!WindowShouldClose()) {
     UpdateMusicStream(music);
 
-    if (IsKeyPressed(KEY_SPACE)) {
-      if (IsMusicStreamPlaying(music)) {
-        PauseMusicStream(music);
-      }
-    } else {
-      ResumeMusicStream(music);
-    }
     BeginDrawing();
     ClearBackground(BLACK);
     // float cell_w = (float)GetRenderWidth() / glo_frames_c;
-    for (size_t i = 0; i < glo_frames_c; ++i) {
-      int16_t sample = *(int16_t *)&glo_frames[i];
-      printf("%d\n", sample);
+    for (size_t i = 0; i < glo_frames_c; i++) {
+      float samps = glo_frames[i];
+      printf("%f", samps);
     }
+
+    printf(" ");
     EndDrawing();
   }
   return 0;
